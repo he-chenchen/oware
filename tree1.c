ddfa7ae78ff2532e734abef3550758181e62a997
chenchen <cche_swu@163.com>
2018-12-02 22:41:09 +0100

    I use the structure to represent the position.

--- tree1.c
+++ tree1.c
@@ -6,38 +6,99 @@ struct position{
 	int cell_computer[6];
 	int cell_player[6];
 	
-	bool isComputer;//who's turn
+	int isComputer;//who's turn
 	int seeds_player; //seeds taken by player
 	int seeds_computer;
 };
 
-position init_Pos(){
-	struct position iniposition;
-	iniposition.cell_computer={4,4,4,4,4,4};
-	iniposition.cell_player={4,4,4,4,4,4};
-	
-	iniposition.isComputer=1;
-	iniposition.seeds_player=0;
-	iniposition.seeds_computer=0;
-	return iniposition;
-};
+void init_Pos(struct position * p){
 
-position play(state,movehole,isComputer){
+	//initialization for the game
+	for(int i=0;i<6;++i){
+		(p->cell_computer)[i]=4;
+		(p->cell_player)[i]=4;
+	}
+	//who's turn, computer or player
+	p->isComputer=1;
+	//initial number of seeds taken by player or computer
+	p->seeds_player=0;
+	p->seeds_computer=0;
+}
+
+//*nextstate
+void playMove(struct position * state,int movehole,int isComputer){
 	int sowseed;
 	if(isComputer){
-		sowseed=state.cell_computer[movehole];
-		index=movehole+sowseed;
-		for (int i = 0; i < count; ++i)
+		//Computer's turn
+		printf("hahahahahahah\n");
+		sowseed=(state->cell_computer)[movehole];
+		for (int i = 0; i < sowseed; ++i)
 		{
-			/* code */
+			int index=movehole+i+1;
+			if(index>5){
+				if(index>11){
+					index=index %12;
+					(state->cell_computer)[index]+=1;
+				}
+				index=index % 6;
+				(state->cell_player)[index]+=1;
+			}else{
+				(state->cell_computer)[index]+=1;
+			}
 		}
-	}else{
-		sowseed=state.cell_player[movehole];
-		
+		(state->cell_computer)[movehole]=0;
+	}
+	else{
+		printf("hahhaha\n");
+		sowseed=(state->cell_player)[movehole];
+		printf("movehole is: %d\n", movehole);
+		printf("sowseed is: %d\n", sowseed);
+
+		for(int i=0; i< sowseed;++i){
+			int index=movehole+i+1;
+			if(index>5){
+				if(index>11){
+					index=index % 12;
+					(state->cell_player)[index]+=1;
+				}
+				index= index % 6;
+				(state->cell_computer)[index]+=1;
+			}else{
+				(state->cell_player)[index]+=1;
+			}
+		}
+		(state->cell_player)[movehole]=0;
 	}
 }
 
 void main(){
-	init_Pos()；
+	struct position pos;
+	struct position * po = & pos;
+	init_Pos(po);
+	for (int i = 0; i < 6; ++i)
+	{
+		printf("cell_computer [%d] is: %d\n ", i,(po->cell_computer)[i] );
+
+	}
+	for (int i = 0; i < 6; ++i)
+	{
+		printf("cell_player [%d] is: %d\n ", i,(po->cell_player)[i] );
+
+	}
+	int movehole = 2; // I want to move hole 4 (starts from 0)
+	int isComputer=0; //computer first
+	playMove(po,movehole,isComputer);
+
+	printf("after move hole %d: \n", movehole);
+	for (int i = 0; i < 6; ++i)
+	{
+		printf("cell_computer [%d] is: %d\n ", i,(po->cell_computer)[i] );
+
+	}
+	for (int i = 0; i < 6; ++i)
+	{
+		printf("cell_player [%d] is: %d\n ", i,(po->cell_player)[i] );
+
+	}
 
 }
